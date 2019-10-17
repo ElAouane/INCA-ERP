5542315f2829730588253cb2bd23a65865e7d12f
Hamza El Aouane <hamzagamedev@hotmail.com>
2019-10-17 04:44:13 +0100

    Added Edit and Task View

--- Appuntamenti/Pages/AdminTask/Create.cshtml.cs
+++ Appuntamenti/Pages/AdminTask/Create.cshtml.cs
@@ -11,12 +11,37 @@ namespace Appuntamenti.Pages.AdminTask
 {
     public class CreateModel : PageModel
     {
+
+        private readonly ApplicationDbContext _db;
+
+        public CreateModel(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+
+
+
         //RETRIEVE THE INFORMATION
         //FROM THE ADMINTASK TO PASS THEM THAN TO CREATE.CS
+        [BindProperty]
         public AdminTasks AdminTask { get; set; }
         public IActionResult OnGet()
         {
             return Page();
+        }
+
+        //ADD POST HANDLER
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+            _db.AdminTasks.Add(AdminTask);
+            await _db.SaveChangesAsync();
+
+            return RedirectToPage("Tasks");
         }
     }
 }
---
+++ Appuntamenti/Pages/AdminTask/Edit.cshtml.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Appuntamenti.Data;
+using Appuntamenti.Models;
+
+namespace Appuntamenti.Pages.AdminTask
+{
+    public class EditModel : PageModel
+    {
+        private readonly ApplicationDbContext _db;
+
+        public EditModel(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        [BindProperty]
+        public AdminTasks AdminTask { get; set; }
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            AdminTask = await _db.AdminTasks.FirstOrDefaultAsync(m => m.Id == id);
+
+            if (AdminTask == null)
+            {
+                return NotFound();
+            }
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            //This Code will optimize the Server.
+            //If we have hundreds of properties, this will modify
+            //Only the properties we want without going
+            //Trough all the database.
+            var taskFromDb = await _db.AdminTasks.FirstOrDefaultAsync(s => s.Id == AdminTask.Id);
+            taskFromDb.Name = AdminTask.Name;
+            taskFromDb.Description = AdminTask.Description;
+            await _db.SaveChangesAsync();
+
+
+            return RedirectToPage("./Tasks");
+        }
+
+    }
+}
