f3a5a023be8e878d7d6ca070f57b82b2fad5ca89
Hamza El Aouane <hamzagamedev@hotmail.com>
2019-10-23 03:15:30 +0100

    Implemented Delete function to User and Tasks

---
+++ Appuntamenti/Pages/AdminTask/Delete.cshtml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Appuntamenti.Data;
+using Appuntamenti.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Appuntamenti.Pages.AdminTask
+{
+    public class DeleteModel : PageModel
+    {
+
+        private readonly ApplicationDbContext _db;
+
+        public DeleteModel(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public AdminTasks Tasks { get; set; }
+
+        public IActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            AdminTasks Tasks = _db.AdminTasks.Find(id);
+
+            if(Tasks == null)
+            {
+                return NotFound();
+            }
+
+            return Page();
+        }
+
+        //[HttpPost, ActionName("Delete")]
+
+        public async Task<IActionResult> OnGetAsync(int id)
+        {
+            AdminTasks Tasks = await _db.AdminTasks.FindAsync(id);
+            _db.AdminTasks.Remove(Tasks);
+            await _db.SaveChangesAsync();
+            return RedirectToPage("Tasks");
+
+        }
+    }
+}
---
+++ Appuntamenti/Pages/Users/Delete.cshtml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Appuntamenti.Data;
+using Appuntamenti.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+
+namespace Appuntamenti.Pages.Users
+{
+    public class DeleteModel : PageModel
+    {
+        private readonly ApplicationDbContext _db;
+
+        public DeleteModel(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        [BindProperty]
+        public ApplicationUser DeleteUser { get; set; }
+
+        
+        public async Task<IActionResult> OnGetAsync(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+            DeleteUser = await _db.ApplicationUser.FirstOrDefaultAsync(u => u.Id == id);
+            _db.ApplicationUser.Remove(DeleteUser);
+            await _db.SaveChangesAsync();
+
+            return RedirectToPage("../Users/Index");
+
+        }
+    }
+}
