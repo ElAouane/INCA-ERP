96f7119695c60283125478a4d154fa41eea278e4
Hamza El Aouane <hamzagamedev@hotmail.com>
2019-10-25 02:54:35 +0100

    Implemented User Details

---
+++ Appuntamenti/Pages/Users/Details.cshtml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Appuntamenti.Data;
+using Appuntamenti.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace Appuntamenti.Pages.Users
+{
+   
+
+    public class DetailsModel : PageModel
+    {
+        private readonly ApplicationDbContext _db;
+
+        public DetailsModel(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        [BindProperty]
+        public IList<ApplicationUser> DetailUser { get; set; }
+        public async Task<IActionResult> OnGet(string id)
+        {
+
+            if (id == null)
+            {
+                return NotFound();
+            }
+            //DetailUser = await _db.ApplicationUser.FirstOrDefaultAsync(m => m.Id == id);
+            DetailUser = await _db.ApplicationUser.Where(m => m.Id == id).ToListAsync();
+
+
+
+           
+            return Page();
+        }
+        
+
+
+    }
+}
